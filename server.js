bf29dcce058927fadced88a4f26d320bc91cad1f
Tommy Danielsson <tdserviceco@gmail.com>
2021-03-18 08:44:27 +0100

    added mysql and mongoDB support

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
+const cors = require('cors')
+app.use(cors())
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get('/ping', (req, res) => {
+  return res.send('pong');
 });
-app.get('/*', function (req, res) {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
