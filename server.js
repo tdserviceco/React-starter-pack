563ef4a7fdd9e6f62536e1067b53e8aea161fa60
Tommy Danielsson <tdserviceco@gmail.com>
2021-03-25 20:29:58 +0100

    added dotenv

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 
 app.use(cors())
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
