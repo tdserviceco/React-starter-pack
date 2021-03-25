7d94db47e53a74b11058650bead254ee43bd06c1
Tommy Danielsson <tdserviceco@gmail.com>
2021-03-25 20:20:24 +0100

    forgot to add cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
-const cors = require('cors')
+
 app.use(cors())
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
