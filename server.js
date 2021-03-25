1f2b51abf015bb2199c45d0df031d5bdf46ab2a7
Tommy Danielsson <tdserviceco@gmail.com>
2021-03-25 20:26:33 +0100

    added dotenv

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config;
+dotenv.config();
+
 const port = process.env.PORT || 8080;
 const app = express();
 // the __dirname is the current directory from where the script is running
